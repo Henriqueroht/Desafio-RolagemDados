479f8c95f9b1cf91f263efc072b9d543061d935e
Henrique Muller Roht <henrique.roht@conectt.com.br>
2021-07-08 17:30:40 -0300

    Criando projeto desaafio-RolagemDados

---
+++ DadosRolagem/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DadosRolagem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
