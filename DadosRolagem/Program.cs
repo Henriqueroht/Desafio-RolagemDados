a5723ad0cfb6eef543e4dce6d7b81eb75773516e
Henrique Muller Roht <henrique.roht@conectt.com.br>
2021-07-15 17:01:04 -0300

    v2

--- DadosRolagem/Program.cs
+++ DadosRolagem/Program.cs
@@ -6,45 +6,55 @@ namespace DadosRolagem
     {
         static void Main()
         {
-            Console.WriteLine("Digite o número de lados do seu dado. Deve ser entre 4 e 100.");
-            int numeroDeLados = Convert.ToInt32(Console.ReadLine());
-            
-            if (numeroDeLados > 100)
+            int numeroDeLados = PerguntaERecebeNumeroDeLados();
+
+            if (numeroDeLados > 100 || numeroDeLados < 4)
             {
-                Console.WriteLine("O número de lados não pode ser maior que 100");
-                PulaLinha(2);
+                ExibeMensagemDeNumeroNaoPermitido(numeroDeLados);
                 Main();
 
-            }
-            else if (numeroDeLados < 4)
-            {
-                Console.WriteLine("O número de lados não pode ser menor que 4");
-                PulaLinha(2);
-                Main();
-            } 
+            }           
             else
             {
-                int numeroRolado = RolarDado(numeroDeLados);                
+                int numeroRolado = RolarDado(numeroDeLados);
 
                 if (numeroRolado % 2 == 0)
-                {                    
+                {
                     ImprimeNumeroPar(numeroRolado);
                 }
                 else
                 {
                     ImprimeNumeroImpar(numeroRolado);
                 }
-            }     
+            }
 
+        }
+
+        private static void ExibeMensagemDeNumeroNaoPermitido(int numeroDeLados)
+        {
+            if (numeroDeLados > 100)
+                Console.WriteLine("O número de lados não pode ser maior que 100");
+
+            if (numeroDeLados < 4)
+                Console.WriteLine("O número de lados não pode ser menor que 4");
+
+            PulaLinha(2);
+        }
+
+        private static int PerguntaERecebeNumeroDeLados()
+        {
+            Console.WriteLine("Digite o número de lados do seu dado. Deve ser entre 4 e 100.");
+            int numeroDeLados = Convert.ToInt32(Console.ReadLine());
+            return numeroDeLados;
         }
 
         private static void ImprimeNumeroImpar(int numeroRolado)
         {
             PulaLinha(1);
             Console.WriteLine($"O número rolado é Impar: {numeroRolado}");
-            PulaLinha(1);
-            Console.WriteLine("Jogue novamente!");
             PulaLinha(2);
+            Console.WriteLine("******  Jogue novamente!  ******");
+            PulaLinha(1);
             Main();
         }
 
@@ -52,9 +62,9 @@ namespace DadosRolagem
         {
             PulaLinha(1);
             Console.WriteLine($"O número rolado é Par: {numeroRolado}");
-            PulaLinha(1);
-            Console.WriteLine("Jogue novamente!");
             PulaLinha(2);
+            Console.WriteLine("******  Jogue novamente!  ******");
+            PulaLinha(1);
             Main();
         }
 
