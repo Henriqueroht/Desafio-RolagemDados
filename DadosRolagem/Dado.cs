0727519d3fdda3c6b25a586284b70751a03b9b48
Henrique Muller Roht <henrique.roht@conectt.com.br>
2021-07-29 01:59:38 -0300

    Vários dados

---
+++ DadosRolagem/Dado.cs
@@ -0,0 +1,25 @@
+﻿
+
+using System;
+
+namespace DadosRolagem
+{
+    class Dados
+    {
+        public int QuantidadeDeLados { get; set; }
+        public int QuantidadeDeDados { get; set; }
+        public int TotalRolado { get; set; }
+        public Dado[] Dado { get; set; }
+    }
+    class Dado
+    {
+        public int QuantidadeDeLados { get; set; }
+        public int NumeroRolado { get; set; }
+
+        public void RolarDado()
+        {
+            Random rd = new Random();
+            NumeroRolado = rd.Next(1, QuantidadeDeLados + 1);
+        }
+    }
+}
--- DadosRolagem/Program.cs
+++ DadosRolagem/Program.cs
@@ -6,28 +6,74 @@ namespace DadosRolagem
     {
         static void Main()
         {
-            int numeroDeLados = PerguntaERecebeNumeroDeLados();
+            Dados dados =  new Dados();
 
-            if (numeroDeLados > 100 || numeroDeLados < 4)
+            dados.QuantidadeDeLados = PerguntaERecebeNumeroDeLados();
+
+            if (dados.QuantidadeDeLados > 100 || dados.QuantidadeDeLados < 4)
             {
-                ExibeMensagemDeNumeroNaoPermitido(numeroDeLados);
+                ExibeMensagemDeNumeroNaoPermitido(dados.QuantidadeDeLados);
                 Main();
-
             }           
             else
             {
-                int numeroRolado = RolarDado(numeroDeLados);
+                Console.WriteLine("Quantos dados deseja rolar?");
+                dados.QuantidadeDeDados = Convert.ToInt32(Console.ReadLine());
 
-                if (numeroRolado % 2 == 0)
+                RolaTodosDados(dados);
+                ImprimeSomaDosResultados(dados);
+
+                Restart();
+            }
+
+        }
+
+        private static void ImprimeSomaDosResultados(Dados dados)
+        {
+            string resumoRolagem = "";
+            for (int i = 0; i < dados.QuantidadeDeDados; i++)
+            {
+                if (i == 0)
+                    resumoRolagem = dados.Dado[i].NumeroRolado.ToString();
+                if (i != 0)
+                    resumoRolagem = resumoRolagem + ", " + dados.Dado[i].NumeroRolado.ToString();
+            }
+
+            Console.WriteLine($"Resumo das rolagens: {resumoRolagem}");
+            Console.WriteLine($"A soma dos resultados de todos os dados rolados é {dados.TotalRolado}.");
+        }
+
+        private static void Restart()
+        {
+            PulaLinha(2);
+            Console.WriteLine("******  Jogue novamente!  ******");
+            PulaLinha(1);
+            Main();
+        }
+
+        private static void RolaTodosDados(Dados dados)
+        {
+            dados.Dado = new Dado[dados.QuantidadeDeDados];
+            PulaLinha(1);
+            for (int i = 0; i < dados.QuantidadeDeDados; i++)
+            {
+                Console.WriteLine($"Rolando dado {(i + 1)}.");
+
+                Dado dado = new Dado();
+                dado.QuantidadeDeLados = dados.QuantidadeDeLados;
+                dado.RolarDado();
+
+                if (dado.NumeroRolado % 2 == 0)
                 {
-                    ImprimeNumeroPar(numeroRolado);
+                    ImprimeNumeroPar(dado.NumeroRolado);
                 }
                 else
                 {
-                    ImprimeNumeroImpar(numeroRolado);
+                    ImprimeNumeroImpar(dado.NumeroRolado);
                 }
+                dados.Dado[i] = dado;
+                dados.TotalRolado +=  dado.NumeroRolado;
             }
-
         }
 
         private static void ExibeMensagemDeNumeroNaoPermitido(int numeroDeLados)
@@ -50,37 +96,22 @@ namespace DadosRolagem
 
         private static void ImprimeNumeroImpar(int numeroRolado)
         {
-            PulaLinha(1);
             Console.WriteLine($"O número rolado é Impar: {numeroRolado}");
-            PulaLinha(2);
-            Console.WriteLine("******  Jogue novamente!  ******");
-            PulaLinha(1);
-            Main();
+            PulaLinha(1);          
         }
 
         private static void ImprimeNumeroPar(int numeroRolado)
         {
-            PulaLinha(1);
             Console.WriteLine($"O número rolado é Par: {numeroRolado}");
-            PulaLinha(2);
-            Console.WriteLine("******  Jogue novamente!  ******");
-            PulaLinha(1);
-            Main();
-        }
-
-        public static int RolarDado(int numeroDeLados)
-        {
-            Random rd = new Random();
-            int numeroRolado = rd.Next(1, numeroDeLados + 1);
-            return numeroRolado;
+            PulaLinha(1);            
         }
 
         static void PulaLinha(int numeroDeLinhas)
         {
            for (int i = 0; i < numeroDeLinhas; i++)
-            {
+           {
                 Console.WriteLine(" ");
-            }
+           }
             
         }
     }
